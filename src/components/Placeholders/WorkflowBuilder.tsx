1426abc2c0452beda2a7e2fd236aedc4d07c6105
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-10-18 14:41:39 +0530

    Updated WorkflowBuilder.tsx

--- src/components/Placeholders/WorkflowBuilder.tsx
+++ src/components/Placeholders/WorkflowBuilder.tsx
@@ -59,9 +59,7 @@ const WorkflowBuilder: React.FC<WorkflowBuilderProps> = ({ userId, roleName, all
       const data = await pesaWorkOperations.getAll();
 
       let allowedVillageIds = allVillageData
-        .filter(v =>
-          !['district', 'developer', 'super admin'].includes(roleName?.trim().toLowerCase()) && userId
-            ? (v.tal_user_access === userId || v.gram_user_access === userId)
+        .filter(v => roleName !== 'district' && userId
             ? (v.tal_user_access === userId || v.gram_user_access === userId)
             : true
         )
