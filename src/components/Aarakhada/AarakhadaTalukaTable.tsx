9b2777fc2bccd51dc347e61dfe9aef1daf6382a7
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-10-18 14:44:16 +0530

    Updated AarakhadaTalukaTable.tsx

--- src/components/Aarakhada/AarakhadaTalukaTable.tsx
+++ src/components/Aarakhada/AarakhadaTalukaTable.tsx
@@ -47,7 +47,7 @@ export function AarakhadaTalukaTable({
   }
 
   // Filter works where work.gram_panchayat matches any item in allowedGramPanchayats
-  const filteredWorks = ['district', 'developer', 'super admin'].includes(roleName?.trim().toLowerCase())
+  const filteredWorks = ['district', 'developer', 'super_admin'].includes(roleName?.trim().toLowerCase())
   ? works // show all works for district-level
   : works.filter(work =>
       allowedGramPanchayats.some(
