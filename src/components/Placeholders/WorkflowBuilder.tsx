a6c77d15f76c6e2e3d81b309f65dd7950294d0ab
ZPChandrapur <dev.zpchandrapur@gmail.com>
2025-10-18 14:47:23 +0530

    Updated WorkflowBuilder.tsx

--- src/components/Placeholders/WorkflowBuilder.tsx
+++ src/components/Placeholders/WorkflowBuilder.tsx
@@ -57,27 +57,23 @@ const WorkflowBuilder: React.FC<WorkflowBuilderProps> = ({ userId, roleName, all
     try {
       setLoading(true);
       const data = await pesaWorkOperations.getAll();
-
+ 
       let allowedVillageIds = allVillageData
-       .filter(v => 
-    !['district', 'developer', 'super_admin'].includes(roleName?.trim().toLowerCase()) &&
-    (
-        userId 
+        .filter(v =>
+          !['district', 'developer', 'super_admin'].includes(roleName?.trim().toLowerCase()) && userId
             ? (v.tal_user_access === userId || v.gram_user_access === userId)
             : true
-    )
-)
-.map(v => v.id);
-
-
-      if (!['district', 'developer', 'super_admin'].includes(roleName) && userId) {
+ 
+        )
+        .map(v => v.id);
+ 
+      if (!['district', 'developer', 'super_admin'].includes(roleName?.trim().toLowerCase()) && userId) {
         if (!allowedVillageIds.length) {
           setWorks([]);
           setLoading(false);
           return;
         }
       }
-
       const filteredWorks = data.filter(work => allowedVillageIds.includes(work.village_id));
       setWorks(filteredWorks.filter(work => work.current_status !== 'completed'));
     } catch (error) {
